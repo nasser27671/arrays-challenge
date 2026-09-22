4f3131f9b94eaa4b8e50397134078bd841f3453d
nasser27671 <nvsserthesavage999@gmail.com>
2026-09-22 11:07:41 +0200

    Add files via upload

---
+++ challengeeee.js
@@ -0,0 +1,6 @@
+let names = ["nasser", "younes", "fatima", "taha", "nour", "zineb"];
+for (let i = 0; i < names.length; i++) {
+    if (!names[i].includes("a")) {
+    console.log(names[i])
+    }
+}
